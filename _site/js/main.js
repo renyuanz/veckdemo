c168e4af17ad231922164c38253c77c52214ae08
RenYuan <renyuan.zou@icloud.com>
2015-06-05 18:20:21 +0200

    change mainjs

---
+++ _site/js/main.js
@@ -0,0 +1,16 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+$(document).ready(function() {
+  
+  $(".button-collapse").sideNav();
+
+  $('.about-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 6000
+  });
+});
